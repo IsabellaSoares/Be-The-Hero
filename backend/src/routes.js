4ed515a3e21d5ca4fccb7f837483309f6fd9778e
Isabella <isabella.soares.28@gmail.com>
2020-03-26 20:19:14 -0300

    feat: add database connection

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const routes = express.Router();
 const crypto = require('crypto');
+const connection = require('./database/connection');
 
-routes.post('/ongs', (request, response) => {
+routes.post('/ongs', async (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body;
   const id = crypto.randomBytes(4).toString('HEX');
-	return response.json();
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
