035dc0bc35f0a0c30ae7ceedf3c4bc47011b265a
Isabella <isabella.soares.28@gmail.com>
2020-03-26 18:54:09 -0300

    feat: post request to create ONGS

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const routes = express.Router();
+const crypto = require('crypto');
 
-routes.get('/users/:id', (request, response) => {
-	const routeParams = request.params;
-	console.log('Route params >> ', routeParams);
-
-	const queryParams = request.query;
-	console.log('Query params >> ', queryParams);
-
-	return response.json({
-		evento: 'Semana OmniStack 11.0',
-		aluno: 'Isabella Soares'
-	});
-});
-
-routes.post('/users', (request, response) => {
-	const requestBody = request.body;
-	console.log('Request body >> ', requestBody);
-
-	return response.json({
-		evento: 'Semana OmniStack 11.0',
-		aluno: 'Isabella Soares'
-	});
+routes.post('/ongs', (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+	return response.json();
 });
 
 module.exports = routes;
