72d14abf6fc5b590537a809c99997f82fbbdf5ac
Isabella <isabella.soares.28@gmail.com>
2020-03-26 22:28:12 -0300

    feat: create Logon component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
 import React, { Fragment, useState } from 'react';
-import Header from './Header';
+import Logon from './pages/Logon';
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  function increment () {
-    setCounter(counter + 1);
-  }
-
 	return (
-		<Fragment>
-			<Header>Contador: {counter}</Header>
-			<button onClick={increment}>Incrementar</button>
-		</Fragment>
+		<Logon />
 	);
 }
 
