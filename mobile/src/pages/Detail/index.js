db50c998d642ed75b1c0161fccc4cf95ab8c341e
Isabella <isabella.soares.28@gmail.com>
2020-03-29 12:25:38 -0300

    send Whatsapp message

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Image, Text } from 'react-native';
+import { View, TouchableOpacity, Image, Text, Linking } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as MailComposer from 'expo-mail-composer';
 import { Feather } from '@expo/vector-icons';
@@ -24,7 +24,7 @@ export default function Detail () {
   }
 
   function sendWhatsapp () {
-
+    Linking.openURL(`whatsapp://send?phone=5515981655143&text=${message}`);
   }
 
   return (
@@ -54,7 +54,7 @@ export default function Detail () {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>Whatsapp</Text>
           </TouchableOpacity>
 
